dcddfd841e5153f176a56fc0390f9a65b5a8c56b
ALING <muhmmad.regi@gmail.com>
2026-09-11 15:20:12 -0700

    fix: npm error

--- src/database.config.ts
+++ src/database.config.ts
@@ -1,24 +1,21 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-// Production config (PostgreSQL)
+// Production config (PostgreSQL via Railway proxy)
 const isProduction = process.env.NODE_ENV === 'production' &&
-  !!process.env.DB_HOST &&
-  !!process.env.DB_USER &&
-  !!process.env.DB_PASSWORD &&
-  !!process.env.DB_NAME;
+  !!process.env.DB_HOST;
 
 export const databaseConfig: TypeOrmModuleOptions = isProduction ? {
   type: 'postgres', // Hardcode sebagai workaround TypeScript strict typing
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: ['dist/**/*.entity{.js,.ts}', './src/**/*.entity{.ts,.js}'],
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '27475'), // Railway proxy default port
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'railway',
+  entities: ['dist/**/*.entity{.js,.ts}'],
   synchronize: false,
 } : {
   type: 'better-sqlite3',
   database: 'db.sqlite',
-  entities: ['dist/**/*.entity{.js,.ts}', './src/**/*.entity{.ts,.js}'],
+  entities: ['./src/**/*.entity{.ts,.js}'],
   synchronize: true,
 };
