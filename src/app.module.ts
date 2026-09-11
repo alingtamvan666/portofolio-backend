dbcbcb5c9b95f19f02d6f4568f346f6920c1663e
ALING <muhmmad.regi@gmail.com>
2026-09-11 15:26:53 -0700

    fix: npm error

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,6 @@ import { ProjectModule } from './project/project.module';
 import { SkillModule } from './skill/skill.module';
 import { ContactModule } from './contact/contact.module';
 import { AdminModule } from './admin/admin.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { UploadModule } from './upload/upload.module';
 
 @Module({
